a28fdc8db1e18ead8b24ef78f50f5e875ff5cda2
o63id3 <h.k.h.e.2000@gmail.com>
2022-10-21 21:55:34 +0300

    commit

--- database.sql
+++ database.sql
@@ -43,6 +43,7 @@ CREATE OR REPLACE TABLE `cache` (
 CREATE OR REPLACE TABLE `images` (
   `hash` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
